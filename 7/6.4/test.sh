69cc65e23864069faa6f189f98a957a8369646f6
Chingis Sandanov <chingis@wodby.com>
2017-03-01 14:46:28 +0700

    Solr images for D7/D8

---
+++ 7/6.4/test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! -z $DEBUG ]]; then
+  set -x
+fi
+
+host='solr'
+port='8983'
+
+waitForSolr() {
+    started=0
+
+    for i in {30..0}; do
+        if solr curl -s "http://${host}:${port}"; then
+            started=1
+            break
+        fi
+        echo 'Solr is starting...'
+        sleep 5
+    done
+
+    if [[ "$started" -eq '0' ]]; then
+        echo >&2 'Error. Solr is unreachable.'
+        exit 1
+    fi
+
+    echo 'Solr has started!'
+}
+
+solr() {
+    docker run --rm -i --link "${NAME}":"${host}" "${IMAGE}" "$@"
+}
+
+waitForSolr
+
+echo "Creating new core..."
+solr make core=core1 host=$host port=$port | grep "200 OK"
+echo "Checking if core has been created..."
+solr make ping core=core1 host=$host port=$port | grep "200 OK"
+echo "Reloading core..."
+solr make reload core=core1 host=$host port=$port | grep "200 OK"
+echo "Deleting core..."
+solr make delete core=core1 host=$host port=$port | grep "200 OK"
+echo "Checking if core has been deleted..."
+solr make ping core=core1 host=$host port=$port | grep "404 Not Found"
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# drupal-solr
+# Apache Solr for Drupal docker image 
+
+[![Build Status](https://travis-ci.org/wodby/solr.svg?branch=master)](https://travis-ci.org/wodby/solr)
+[![Docker Pulls](https://img.shields.io/docker/pulls/wodby/solr.svg)](https://hub.docker.com/r/wodby/solr)
+[![Docker Stars](https://img.shields.io/docker/stars/wodby/solr.svg)](https://hub.docker.com/r/wodby/solr)
+
+## Supported tags and respective `Dockerfile` links
+
+- [`8-6.4`, `latest` (*8/6.4/Dockerfile*)](https://github.com/wodby/solr/tree/master/8/6.4/Dockerfile)
+- [`8-6.3` (*8/6.3/Dockerfile*)](https://github.com/wodby/solr/tree/master/8/6.3/Dockerfile)
+- [`8-5.5` (*8/5.5/Dockerfile*)](https://github.com/wodby/solr/tree/master/8/5.5/Dockerfile)
+- [`7-6.4` (*7/6.4/Dockerfile*)](https://github.com/wodby/solr/tree/master/7/6.4/Dockerfile)
+- [`7-6.3` (*7/6.3/Dockerfile*)](https://github.com/wodby/solr/tree/master/7/6.3/Dockerfile)
+- [`7-5.5` (*7/5.5/Dockerfile*)](https://github.com/wodby/solr/tree/master/7/5.5/Dockerfile)
+
+## Actions
+
+See at [wodby/solr](https://github.com/wodby/solr) 
